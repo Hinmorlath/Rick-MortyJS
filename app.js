e052b2810709c5c9255b223917c7bb152a34dd4f
Hinmorlath <josepolomanuel2@gmail.com>
2022-10-18 12:55:36 -0500

    navegacion

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const urlBase = 'https://rickandmortyapi.com/api/character/';
 
-fetch(urlBase)
-    .then(respuesta => {
-        //console.log(respuesta);
-        return respuesta.json();
-    })
+const loadData = (url, page = 1) => {
+    url += `?page=${page}`
+    fetch(url)
+    .then(respuesta => respuesta.json())
     .then(respJson => {
         const info = respJson.info;
         const personajes = respJson.results;
-        console.log(info);
-        console.log(personajes);
+        console.log(info.next);
+        console.log(info.prev);
+        if(!info.prev){
+            document.querySelector('#prev').classList.add('disabled')
+        } else {
+            document.querySelector('#prev').classList.remove('disabled')
+            document.querySelector('#prev').setAttribute('data-id', page - 1)
+        }
+        if(!info.next){
+            document.querySelector('#next').classList.add('disabled')
+        } else {
+            document.querySelector('#next').classList.remove('disabled')
+            document.querySelector('#next').setAttribute('data-id', page + 1)
+        }
         console.log(personajes);
         showCharacters(personajes);
     })
+}
+
+const navegacion = (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('btn')){
+        let page = e.target.getAttribute('data-id');
+        loadData(urlBase, page);
+    }
+}
+
+loadData(urlBase);
+
+document.querySelector('.botones').addEventListener('click', navegacion);
 
 const showCharacters = (personajes) => {
     /*const ul = document.createElement('ul');
@@ -24,6 +48,9 @@ const showCharacters = (personajes) => {
     });
     document.querySelector('#respuesta').appendChild(ul);*/
     const contenedorRespuesta = document.querySelector('#respuesta');
+    while(contenedorRespuesta.firstChild){
+        contenedorRespuesta.remove(contenedorRespuesta.firstChild);
+    }
     personajes.forEach(personaje => {
         contenedorRespuesta.appendChild(creaCard(personaje));
     })
